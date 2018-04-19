8a9a1d55cc29c4ef4a19df43862fdcac09414b63
imrekertesz <ikertesz@student.ethz.ch>
2018-04-19 13:11:23 +0200

    update

--- Models/Model1.py
+++ Models/Model1.py
@@ -1,6 +1,3 @@
-import numpy
-import tensorflow
-import cv2
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Conv2D, MaxPooling2D, Flatten
 from keras.losses import categorical_crossentropy
@@ -8,24 +5,24 @@ from keras.optimizers import sgd
 from keras.callbacks import ModelCheckpoint
 from keras.utils import np_utils
 from sklearn.utils import shuffle
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 import os
 from keras.utils.np_utils import to_categorical
-#import numpy as np
+import numpy as np
 from PIL import Image
-from numpy import *
 
 img_rows, img_cols = 64,64
-
 img_channels = 1
-path_1 = 'training-data'
-imlist = os.listdir(path_1)
+directory = os.path.dirname(__file__)
+data_path = os.path.join(directory,'../training-data/')
+imlist = os.listdir(data_path)
 im1 = (64,64)
-imnbr =len(imlist)
+imnbr = len(imlist)
 
-immatrix = numpy.array([numpy.array(Image.open('training-data' + '/' + im2)).flatten() for im2 in imlist],'f')
+immatrix = np.array([np.array(Image.open(data_path + im2)) for im2 in imlist], 'f')
+print('immatrix shape: ', immatrix.shape)
 
-label= ones((imnbr,),dtype=int)
+label= np.ones((imnbr,),dtype=int)
 label[37:]=0
 label[0:37]=1
 data, Label=shuffle(immatrix,label,random_state=2)
@@ -37,25 +34,31 @@ x_test = x_test.astype('float32')
 print('X_train shape:', x_train.shape)
 print(x_train.shape[0], 'train samples')
 print(x_test.shape[0], 'test samples')
-y_train = np_utils.to_categorical(Y_train, 2)
-y_test = np_utils.to_categorical(Y_test, 2)
+y_train = to_categorical(Y_train, 2)
+y_test = to_categorical(Y_test, 2)
 
+#Define Model parameters
 channels = 32
 kernel_size = (5, 5)
 pool_size = (2, 2)
 learning_rate = 0.01
 batch_size = 32
+epochs = 50
+
+#Define Model
 model = Sequential()
-model.add(Conv2D(channels, kernel_size, strides=(1,1), activation="relu", input_shape=(64,64,1)))
+model.add(Conv2D(channels, kernel_size, strides=(1,1), activation="relu", input_shape=(64,64,3))) #1.convolutional layer
 model.add(MaxPooling2D(pool_size, strides=(1,1)))
-model.add(Conv2D(channels, kernel_size, strides=(1,1), activation="relu", input_shape=(64,64,1)))
+model.add(Conv2D(channels, kernel_size, strides=(1,1), activation="relu", input_shape=(64,64,3))) #2. convolutional layer
 model.add(MaxPooling2D(pool_size, strides=(1,1)))
-model.add(Flatten())
+model.add(Flatten()) #flattens the output of the convolutional layer
 model.add(Dense(500, activation="relu"))
 model.add(Dense(500, activation="relu"))
 model.add(Dense(2, activation="softmax"))
-model.compile(loss=categorical_crossentropy(y_true=), optimizer=sgd(lr=0.01), metrics=['accuracy'])
-filepath = '/home/federico/Desktop/A_iWaldo/Models/fail.hdf5'
-checkpoint = ModelCheckpoint(filepath, monitor='val_atc', verbose=1,save_best_only=True, mode='max')
+model.compile(loss=categorical_crossentropy, optimizer=sgd(lr=0.01), metrics=['accuracy'])
+
+#Create filepath
+filepath = os.path.join(directory,'../Saved_Models/Model1_saved.hdf5')#'/home/federico/Desktop/A_iWaldo/Models/fail.hdf5'
+checkpoint = ModelCheckpoint(filepath, monitor='val_loss', verbose=1,save_best_only=True, mode='max')
 callback_list = [checkpoint]
-model.fit(x_train, y_train, batch_size, epochs= 100,verbose=1,validation_data=(x_test,y_test), callbacks=callback_list)
+model.fit(x_train, y_train, batch_size, epochs,verbose=1,validation_data=(x_test,y_test), callbacks=callback_list)
---
+++ Models/Model2.py
@@ -0,0 +1,77 @@
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Conv2D, MaxPooling2D, Flatten
+from keras.losses import categorical_crossentropy
+from keras.optimizers import sgd
+from keras.callbacks import ModelCheckpoint
+from keras.utils import np_utils
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
+import os
+from keras.utils.np_utils import to_categorical
+from keras.preprocessing.image import ImageDataGenerator
+import numpy as np
+from PIL import Image
+
+#Define Model parameters
+channels = 32
+kernel_size = (5, 5)
+pool_size = (2, 2)
+learning_rate = 0.01
+batch_size = 32
+epochs = 50
+
+
+directory = os.path.dirname(__file__)
+data_path = os.path.join(directory,'../training-data/')
+imlist = os.listdir(data_path)
+imnbr = len(imlist)
+
+# data generator for training set
+train_datagen = ImageDataGenerator(rescale = 1./255,shear_range = 0.2, zoom_range = 0.2,rotation_range=10)
+
+# data generator for test set
+test_datagen = ImageDataGenerator(rescale = 1./255)
+
+# generator for reading train data from folder
+train_generator = train_datagen.flow_from_directory(
+    data_path,
+    target_size = (64, 64),
+    color_mode = 'grayscale',
+    batch_size = batch_size,
+    class_mode = 'binary')
+
+# generator for reading test data from folder
+test_generator = test_datagen.flow_from_directory(
+    'data/test',
+    target_size = (256, 256),
+    color_mode = 'rgb',
+    batch_size = 1,
+    class_mode = 'binary',
+    shuffle = False)
+
+
+
+
+
+#Define Model
+model = Sequential()
+model.add(Conv2D(channels, kernel_size, strides=(1,1), activation="relu", input_shape=(64,64,1))) #1.convolutional layer
+model.add(MaxPooling2D(pool_size, strides=(1,1)))
+model.add(Conv2D(channels, kernel_size, strides=(1,1), activation="relu", input_shape=(64,64,1))) #2. convolutional layer
+model.add(MaxPooling2D(pool_size, strides=(1,1)))
+model.add(Flatten()) #flattens the output of the convolutional layer
+model.add(Dense(500, activation="relu"))
+model.add(Dense(500, activation="relu"))
+model.add(Dense(2, activation="softmax"))
+model.compile(loss=categorical_crossentropy, optimizer=sgd(lr=0.01), metrics=['accuracy'])
+
+#Create filepath
+filepath = os.path.join(directory,'../Saved_Models/Model1_saved.hdf5')#'/home/federico/Desktop/A_iWaldo/Models/fail.hdf5'
+checkpoint = ModelCheckpoint(filepath, monitor='val_loss', verbose=1,save_best_only=True, mode='max')
+callback_list = [checkpoint]
+model.fit_generator(train_generator,
+                    steps_per_epoch = steps,
+                    epochs = 10,
+                    validation_data = validation_generator,
+                    validation_steps = num_val_samples/batch_size)
+#model.fit(x_train, y_train, batch_size, epochs,verbose=1,validation_data=(x_test,y_test), callbacks=callback_list)
